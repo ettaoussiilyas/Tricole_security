ccb6a7ee3bc71472bb75713cdd36e0809035600c
ettaoussiilyas <ettaoussiilyas0.1@gmail.com>
2025-12-23 21:49:05 +0100

    test CI/CD

--- target/classes/db/changelog/changes/003-add-atelier-to-bon-sortie.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset abdellatif:003-add-atelier-to-bon-sortie
-ALTER TABLE bon_sortie ADD COLUMN atelier VARCHAR(100);
-
-
--- rollback ALTER TABLE bon_sortie DROP COLUMN atelier;
-
-
--- target/classes/db/changelog/changes/004-add-montant-total-to-bon-sortie.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeset abdellatif:004-add-montant-total-to-bon-sortie
-ALTER TABLE bon_sortie ADD COLUMN montant_total DECIMAL(19,2);
-
-
--- rollback ALTER TABLE bon_sortie DROP COLUMN montant_total;
